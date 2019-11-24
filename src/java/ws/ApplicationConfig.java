0093b5fa38de54f5b210130c5dd86c7cb71d40e9
Danilo <danilo.davanzzo@me.com>
2019-11-24 20:11:19 -0300

    Habilitação de cors

--- src/java/ws/ApplicationConfig.java
+++ src/java/ws/ApplicationConfig.java
@@ -26,6 +26,7 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(dao.RESTFulCORSFilter.class);
         resources.add(ws.FilmeWS.class);
     }
     
